f15e93ecab65b3d6ab9450407ffbf3687b52b4b9
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-04-02 14:01:39 +0530

    provide app route

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Register from "./components/Register.jsx";
+import Login from "./components/Login.jsx";
+import Url from "./components/Url.jsx";
+import Home from "./components/Home.jsx"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+    ],
+  },
+  {
+    path: "short-url",
+    element: <Url />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={appRouter} />
+  </React.StrictMode>
+);
